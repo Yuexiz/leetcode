90ece1073f0e6857ff1f6117fe897f915ee76a6d
Yuexiz <yuexi.z@gmail.com>
2019-02-20 16:49:58 +0800

    update at 2019-02-20

---
+++ 001-two-sum/two-sum.swift
@@ -0,0 +1,37 @@
+// Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+//
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+//
+// Example:
+//
+//
+// Given nums = [2, 7, 11, 15], target = 9,
+//
+// Because nums[0] + nums[1] = 2 + 7 = 9,
+// return [0, 1].
+//
+//
+//  
+//
+
+
+class Solution {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+    
+    var dic: [Int: Int] = [:]
+    
+    for i in 0..<nums.count {
+        dic[nums[i]] = i
+    }
+    
+    for a in 0..<nums.count {
+        let newValue = target - nums[a]
+        
+        if let j = dic[newValue], j != a {
+            return [a, j]
+        }
+    }
+    
+    return []
+}
+}
